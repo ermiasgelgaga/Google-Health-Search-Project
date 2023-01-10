ba0fd27d5e5f0f9352f4bc424a73e9975c995394
ermias <ermiasgelaye@gmail.com>
2023-01-10 17:05:02 -0500

    master

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import numpy as np
 import os
 import json
-import requests
-import sqlalchemy
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.ext.automap import automap_base
@@ -13,27 +11,47 @@ from config import pg_user, pg_password, db_name
 from flask import Flask, jsonify, render_template, abort, redirect
 from flask_sqlalchemy import SQLAlchemy
 
+
+
 #################################################
 # Database Setup
 ##################################################
 
-engine = create_engine('postgresql://kdojexxzviryyp:64e74879664d1ab6e5f2f2dccd55344d562770cb46a018801a67352779dce8aa@ec2-52-70-86-157.compute-1.amazonaws.com:5432/d29l5bii4auh29')
+#engine = create_engine('postgres://kdojexxzviryyp:64e74879664d1ab6e5f2f2dccd55344d562770cb46a018801a67352779dce8aa@ec2-52-70-86-157.compute-1.amazonaws.com:5432/d29l5bii4auh29')
+
+
 
 
 # checking the table names
-engine.table_names()
+## engine.table_names()
 
 
 #################################################
 # Flask Setup
 #################################################
+
+DATABASE_URL = "postgres://kdojexxzviryyp:64e74879664d1ab6e5f2f2dccd55344d562770cb46a018801a67352779dce8aa@ec2-52-70-86-157.compute-1.amazonaws.com:5432/d29l5bii4auh29"
+
+DATABASE_URL = DATABASE_URL.replace(
+    'postgres://',
+    'postgresql://',
+    1
+)
+
+engine = create_engine(DATABASE_URL)
+engine.table_names()
+
+
+
 app = Flask(__name__)
 
-app.config['SQLAlCHEMY_DATABASE_URI']=os.environ.get('DATABASE_URI','') or "sqlite:///db.sqlite"
+app.config['SQLAlCHEMY_DATABASE_URI']=os.getenv('DATABASE_URI','') or "sqlite:///db.sqlite"
+
 
 app.config['SQLAlCHEMY_TRACK_MODIFICATION']=False
 
 db=SQLAlchemy(app)
+
 
 #################################################
 # Flask Routes
@@ -62,6 +80,7 @@ def searchbyyear():
     df = df.to_json(orient='table')
     result = json.loads(df)
     return jsonify(result)
+
 @app.route('/searchyearandcondition')
 def searchyearandcondition():
     sqlStatement = """
