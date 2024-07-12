07b19f0dcb01edfe3342984acc33201c0e4837d0
ermias <ermiasgelaye@gmail.com>
2024-07-12 17:54:23 -0400

    last
    last

--- app.py
+++ app.py
@@ -23,6 +23,12 @@ DATABASE_URL = DATABASE_URL.replace(
 
 # Create SQLAlchemy engine
 engine = create_engine(DATABASE_URL)
+meta = sqlalchemy.MetaData()
+meta.reflect(bind=engine)
+table_names = meta.tables.keys()
+print(table_names)  # or use the list of table_names as needed
+
+
 
 #################################################
 # Flask Setup
@@ -50,135 +56,164 @@ def home():
 
 @app.route('/searchbyyear')
 def searchbyyear():
-    sql_statement = """
-        SELECT year, SUM("Cancer" + "cardiovascular" + "stroke" + "depression" + "rehab" + "vaccine" + "diarrhea" + "obesity" + "diabetes") AS Searches  
-        FROM search_condition 
-        GROUP BY year
-        ORDER BY year;
+    sqlStatement = """
+   SELECT year, SUM ("Cancer" + "cardiovascular" + "stroke" + "depression" + "rehab" + "vaccine" + "diarrhea" + "obesity" + "diabetes") AS Searches  
+   FROM search_condition 
+   GROUP BY year
+    ORDER BY year;
     """
-    df = pd.read_sql(sql_statement, engine)
+    df = pdsql.read_sql(sqlStatement, engine)
     df.set_index('year', inplace=True)
-    result = df.to_json(orient='table')
-    return result
+    df = df.to_json(orient='table')
+    result = json.loads(df)
+    return jsonify(result)
+
+@app.route('/searchyearandcondition')
+def searchyearandcondition():
+    sqlStatement = """
+    SELECT year, SUM ("Cancer") AS Cancer,SUM ("cardiovascular") As Cardiovascular,SUM ("stroke") As Stroke,SUM ("depression") As Depression,SUM ("rehab") AS Rehab,SUM ("vaccine") AS Vaccine, SUM ("diarrhea") AS Diarrhea, SUM("obesity") AS Obesity, SUM ("diabetes") AS Diabetes    
+    FROM search_condition 
+    GROUP BY year
+    ORDER BY year;
+
+    """
+    df = pdsql.read_sql(sqlStatement, engine)
+    df.set_index('year', inplace=True)
+    df = df.to_json(orient='table')
+    result = json.loads(df)
+    return jsonify(result)
 
 @app.route('/searchbycity')
 def searchbycity():
-    sql_statement = """
-        SELECT l.city, l.postal, l.state, l.latitude, l.longitude, SUM(s."Cancer" + s."cardiovascular" + s."stroke" + s."depression" + s."rehab" + s."vaccine" + s."diarrhea" + s."obesity" + s."diabetes") AS Searches  
-        FROM location l
-        INNER JOIN search_condition s ON s.location_id = l.location_id
-        GROUP BY l.city, l.state, l.postal, l.latitude, l.longitude
-        ORDER BY l.city;
+    sqlStatement = """
+SELECT l.city,l.postal,l.state, l.latitude, l.longitude, SUM (s."Cancer" + s."cardiovascular" + s."stroke" + s."depression" + s."rehab" + s."vaccine" + s."diarrhea" + s."obesity" + s."diabetes") AS Searches  
+FROM location l
+INNER JOIN search_condition s on s.location_id = l.location_id
+GROUP BY l.city,l.state,l.postal, l.latitude, l.longitude
+ORDER BY l.city;
     """
-    df = pd.read_sql(sql_statement, engine)
+    df = pdsql.read_sql(sqlStatement, engine)
     df.set_index('city', inplace=True)
-    result = df.to_json(orient='table')
-    return result
+    df = df.to_json(orient='table')
+    result = json.loads(df)
+    return jsonify(result)
 
 @app.route('/searchbystate')
 def searchbystate():
-    sql_statement = """
-        SELECT l.state, l.postal, SUM(s."Cancer" + s."cardiovascular" + s."stroke" + s."depression" + s."rehab" + s."vaccine" + s."diarrhea" + s."obesity" + s."diabetes") AS Searches  
-        FROM location l
-        INNER JOIN search_condition s ON s.location_id = l.location_id
-        GROUP BY l.state, l.postal;
-    """
-    df = pd.read_sql(sql_statement, engine)
+    sqlStatement = """
+SELECT l.state,l.postal, SUM (s."Cancer" + s."cardiovascular" + s."stroke" + s."depression" + s."rehab" + s."vaccine" + s."diarrhea" + s."obesity" + s."diabetes") AS Searches  
+FROM location l
+INNER JOIN search_condition s on s.location_id = l.location_id
+GROUP BY l.state,l.postal;
+ """
+    df = pdsql.read_sql(sqlStatement, engine)
     df.set_index('state', inplace=True)
-    result = df.to_json(orient='table')
-    return result
+    df = df.to_json(orient='table')
+    result = json.loads(df)
+    return jsonify(result)
 
 @app.route('/bystateandyear')
 def bylocationandyear():
-    sql_statement = """
-        SELECT l.state, l.latitude, l.longitude, s.year, SUM(s."Cancer" + s."cardiovascular" + s."stroke" + s."depression" + s."rehab" + s."vaccine" + s."diarrhea" + s."obesity" + s."diabetes") AS Searches  
-        FROM location l
-        INNER JOIN search_condition s ON s.location_id = l.location_id
-        GROUP BY l.state, l.latitude, l.longitude, s.year
-        ORDER BY s.year;
+    sqlStatement = """
+ SELECT l.state, l.latitude, l.longitude,s.year, SUM (s."Cancer" + s."cardiovascular" + s."stroke" + s."depression" + s."rehab" + s."vaccine" + s."diarrhea" + s."obesity" + s."diabetes") AS Searches  
+FROM location l
+INNER JOIN search_condition s on s.location_id = l.location_id
+GROUP BY l.state, l.latitude, l.longitude, s.year
+ORDER BY s.year;
     """
-    df = pd.read_sql(sql_statement, engine)
+    df = pdsql.read_sql(sqlStatement, engine)
     df.set_index('state', inplace=True)
-    result = df.to_json(orient='table')
-    return result
+    df = df.to_json(orient='table')
+    result = json.loads(df)
+    return jsonify(result)
 
 @app.route('/casesleadingdeath')
 def casesleadingdeath():
-    sql_statement = """
-        SELECT * FROM leading_causes_of_death;
+    sqlStatement = """
+    SELECT * FROM leading_causes_of_death;
+
     """
-    df = pd.read_sql(sql_statement, engine)
+    df = pdsql.read_sql(sqlStatement, engine)
     df.set_index('year', inplace=True)
-    result = df.to_json(orient='table')
-    return result
+    df = df.to_json(orient='table')
+    result = json.loads(df)
+    return jsonify(result)
 
 @app.route('/allsearchrecord')
 def allsearchrecord():
-    sql_statement = """
-        SELECT *
-        FROM location l
-        INNER JOIN search_condition s ON s.location_id = l.location_id
-        ORDER BY s.year;
+    sqlStatement = """
+    SELECT *
+    FROM location l
+    INNER JOIN search_condition s on s.location_id = l.location_id
+    ORDER BY s.year;
     """
-    df = pd.read_sql(sql_statement, engine)
+    df = pdsql.read_sql(sqlStatement, engine)
     df.set_index('year', inplace=True)
-    result = df.to_json(orient='table')
-    return result
+    df = df.to_json(orient='table')
+    result = json.loads(df)
+    return jsonify(result)
 
 @app.route('/location')
 def location():
-    sql_statement = """
-        SELECT * FROM location;
+    sqlStatement = """
+    SELECT * FROM location;
     """
-    df = pd.read_sql(sql_statement, engine)
+    df = pdsql.read_sql(sqlStatement, engine)
     df.set_index('location_id', inplace=True)
-    result = df.to_json(orient='table')
-    return result
+    df = df.to_json(orient='table')
+    result = json.loads(df)
+    return jsonify(result)
 
 @app.route('/conditions')
 def conditions():
-    sql_statement = """
-        SELECT * FROM search_condition;
+    sqlStatement = """
+    SELECT * FROM search_condition;
     """
-    df = pd.read_sql(sql_statement, engine)
+    df = pdsql.read_sql(sqlStatement, engine)
     df.set_index('location_id', inplace=True)
-    result = df.to_json(orient='table')
-    return result
+    df = df.to_json(orient='table')
+    result = json.loads(df)
+    return jsonify(result)
 
 @app.route('/mostsserached')
 def mostsserached():
-    sql_statement = """
-        SELECT l.state, SUM("Cancer") AS Cancer, SUM("cardiovascular") AS Cardiovascular, SUM("stroke") AS Stroke, SUM("depression") AS Depression, SUM("rehab") AS Rehab, SUM("vaccine") AS Vaccine, SUM("diarrhea") AS Diarrhea, SUM("obesity") AS Obesity, SUM("diabetes") AS Diabetes 
-        FROM location l
-        INNER JOIN search_condition s ON s.location_id = l.location_id
-        GROUP BY l.state
-        ORDER BY Cancer DESC, Cardiovascular DESC, Stroke DESC, Depression DESC, Rehab DESC, Vaccine DESC, Diarrhea DESC, Diabetes DESC
-        LIMIT 10; 
+    sqlStatement = """
+    SELECT l.state, SUM ("Cancer") AS Cancer,SUM ("cardiovascular") As Cardiovascular,SUM ("stroke") As Stroke,SUM ("depression") As Depression,SUM ("rehab") AS Rehab,SUM ("vaccine") AS Vaccine, SUM ("diarrhea") AS Diarrhea, SUM("obesity") AS Obesity, SUM ("diabetes") AS Diabetes 
+    FROM location l
+    INNER JOIN search_condition s on s.location_id = l.location_id
+    GROUP BY state
+    order by Cancer desc, Cardiovascular desc,Stroke desc,Depression desc,Rehab desc,Vaccine desc, Diarrhea desc, Diabetes desc, Obesity desc
+    LIMIT 10; 
     """
-    df = pd.read_sql(sql_statement, engine)
+    df = pdsql.read_sql(sqlStatement, engine)
     df.set_index('state', inplace=True)
-    result = df.to_json(orient='table')
-    return result
+    df = df.to_json(orient='table')
+    result = json.loads(df)
+    return jsonify(result)
 
 @app.route('/totalcondition')
 def totalcondition():
-    sql_statement = """
-        SELECT SUM("Cancer") AS Cancer, SUM("cardiovascular") AS Cardiovascular, SUM("stroke") AS Stroke, SUM("depression") AS Depression, SUM("rehab") AS Rehab, SUM("vaccine") AS Vaccine, SUM("diarrhea") AS Diarrhea, SUM("obesity") AS Obesity, SUM("diabetes") AS Diabetes    
-        FROM search_condition;
+    sqlStatement = """
+    SELECT SUM ("Cancer") AS Cancer,SUM ("cardiovascular") As Cardiovascular,SUM ("stroke") As Stroke,SUM ("depression") As Depression,SUM ("rehab") AS Rehab,SUM ("vaccine") AS Vaccine, SUM ("diarrhea") AS Diarrhea, SUM("obesity") AS Obesity, SUM ("diabetes") AS Diabetes    
+    FROM search_condition 
     """
-    df = pd.read_sql(sql_statement, engine)
-    result = df.to_json(orient='table')
-    return result
+    df = pdsql.read_sql(sqlStatement, engine)
+    df.set_index('cancer', inplace=True)
+    df = df.to_json(orient='table')
+    result = json.loads(df)
+    return jsonify(result)
 
 @app.route('/totaldeathcase')
 def totaldeathcase():
-    sql_statement = """
-        SELECT SUM("Accidents") AS Accidents, SUM("Alzheimer") AS Alzheimer, SUM("Cerebrovascular") AS Cerebrovascular, SUM("Diabetes") AS Diabetes, SUM("Diseases_of_heart") AS Diseases_of_Heart, SUM("Influenza_and_pneumonia") AS Influenza_and_Pneumonia, SUM("Malignant_neoplasms") AS Malignant_neoplasms, SUM("Nephrosis") AS Nephrosis, SUM("Suicide") AS Suicide, SUM("Respiratory") AS Respiratory   
-        FROM leading_causes_of_death;
+    sqlStatement = """
+    SELECT SUM ("Accidents") AS Accidents,SUM ("Alzheimer") As Alzheimer,SUM ("Cerebrovascular") As Cerebrovascular,SUM ("Diabetes") As Diabetes,SUM ("Diseases_of_heart") AS Diseases_of_Heart, SUM ("Influenza_and_pneumonia") AS Influenza_and_Pneumonia, SUM ("Malignant_neoplasms") AS Malignant_eoplasms, SUM("Nephrosis") AS Nephrosis, SUM ("Suicide") AS Suicide,SUM ("Respiratory") AS Respiratory   
+    FROM leading_causes_of_death
     """
-    df = pd.read_sql(sql_statement, engine)
-    result = df.to_json(orient='table')
-    return result
+    df = pdsql.read_sql(sqlStatement, engine)
+    df.set_index('alzheimer', inplace=True)
+    df = df.to_json(orient='table')
+    result = json.loads(df)
+    return jsonify(result)
 
 if __name__ == '__main__':
     app.run(debug=True)
