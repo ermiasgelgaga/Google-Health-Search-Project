1521355474311eb1b43399a3434a81a3e990d77f
ermias <ermiasgelaye@gmail.com>
2024-07-12 18:23:03 -0400

    last
    last

--- Data/database/load_in_to_db/config.py
+++ Data/database/load_in_to_db/config.py
@@ -1,9 +1,10 @@
+
 # Configuration file for database credentials
 
 pg_user = 'avnadmin'
-pg_password = 'AVNS_H8UldLoBDWmhUDQHibe'
+pg_password = 'AVNS_2JZqm8RJuAhCdk4c6eK'
 db_name = 'defaultdb'
-pg_host = 'pg-1ff11395-ermiasgelaye-d9b4.h.aivencloud.com:26131'
+pg_host = 'mydatabase-ermiasgelaye-d9b4.c.aivencloud.com:26131'
 
 # Create the DATABASE_URL string
 DATABASE_URL = f"postgresql://{pg_user}:{pg_password}@{pg_host}/{db_name}"
